ac78d0a41500c5c1d9df4133b3176f6377a0ec49
Ege Tuncoz <tuncozege@gmail.com>
2017-01-06 15:24:05 +0000

    Added stuff

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 var express = require("express");
 var app = express();
+var moment = require("moment");
+var http = require("http");
 
 app.get('/',function(req,res){
-    res.send('whad up');
+   res.sendfile(__dirname + '/index.html');
 });
 
-app.listen(8080,function(){
+app.get('/:time',function(req,res){
+    var displayObj = {
+        "unix": 0,
+        "natural": ""
+    }
+    var date;
+    
+    if(/\d{8,13}/.test(req.params.time)){ //if unix..
+        date = moment(req.params.time,'X');
+    }else{
+        date = moment(req.params.time,'MMMM DD, YYYY');
+    }
+    
+    if(date.isValid()){
+        displayObj.unix = date.format('X');
+        displayObj.natural = date.format('MMMM DD, YYYY');
+        res.send(displayObj);
+    }else{
+        displayObj.unix = null;
+        displayObj.natural = null;
+        res.send(displayObj);
+    }
+    
+});
+
+http.createServer(app).listen(8080,function(){
     console.log("server is listening to *8080");
 });
